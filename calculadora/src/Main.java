d7e8f949bf8511d02ba2f521828387fcac92bf8a
vinicius <144129498+viniboy2@users.noreply.github.com>
2025-01-02 14:48:21 -0300

    2
    calculadora simples utilizando o modelo estruturado depois podendo ser substituído por poo

--- calculadora/src/Main.java
+++ calculadora/src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+        double numero1;
+        double numero2; //declaração dos dois numeros a serem utilizados
+        // Solicita o número ao usuário
+        System.out.println("Digite um número: ");
+        numero1 = scanner.nextDouble(); // Lê o número e atribui a 'numero1'
+        System.out.println("Digite um número: ");
+        numero2 = scanner.nextDouble(); // Lê o número e atribui a 'numero2'
+        System.out.println("selecione uma opreração que deseja fazer 1= soma , 2= diferença entre numero 1 e numero2");
+        System.out.println("3= multiplicação, 4= divisão )");
+        opcao = scanner.nextInt();
+        switch(opcao){
+            case 1:
+                System.out.println("a soma dos números é :"+ (numero1+numero2));
+                break;
+            case 2:
+                System.out.println("a diferença dos números é :"+ (numero1-numero2));
+                break;
+                case 3:
+                System.out.println("a multiplicação dos números é :"+ (numero1*numero2));
+                break;
+            case 4:
+                System.out.println("a divição do primeiro pelo segundo número é :"+ (numero1/numero2));
+                break;
         }
+        scanner.close(); // Fecha o Scanner
     }
 }
