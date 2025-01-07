586baa7c9cc677702fcf53d797b460c4a5afc168
vinicius <144129498+viniboy2@users.noreply.github.com>
2025-01-07 18:44:57 -0300

    update

--- mercado projeto3/src/DAO/ConexaoDAO.java
+++ mercado projeto3/src/DAO/ConexaoDAO.java
@@ -1,4 +1,5 @@
-package Dao;
+package DAO;
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
---
+++ mercado projeto3/src/TesteConexao/Main.java
@@ -0,0 +1,19 @@
+package TesteConexao;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        public class ExecutarTeste {
+            public static void main(String[] args) {
+                TesteConexao teste = new TesteConexao();
+                teste.testarConexao();
+            }
+        }
+
+
+
+        }
+    }
